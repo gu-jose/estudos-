078436d97f0c2549f3f452748b2ef5bf1a6cb235
jose augusto <eduardbatistasantos@gmail.com>
2022-10-24 23:35:31 -0300

    adcionando atributos a classe animais

--- aulaNatali/src/aulaNatali/Animal.java
+++ aulaNatali/src/aulaNatali/Animal.java
@@ -1,5 +1,16 @@
 package aulaNatali;
 
-public class Animal {
+public class Animal { 
+	
+	//  atributo , cor, peso, tamanho e  especie
+	
+	private String cor;
+	private  float  peso;
+	private double  tamanho;
+	private String especie;
+	
+	
+	
+	
 
 }
